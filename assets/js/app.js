f16d9352f105df704db1f97b805f3370a2a5006c
Mohammed Mahdy <mohamed.mahdy@sphinxinfotech.net>
2017-06-12 11:54:01 +0200

    init, create template and apply highlight filter

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var myApp = angular.module('MyApp', ['ngSanitize']);
+
+myApp.controller('MainController', function ($scope) {
+    $scope.msg = 'Hello Angular UI';
+});
+
+myApp.controller('HighlightController', function ($scope) {
+    $scope.text = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+});
+
+/**
+ * Wraps the
+ * @param text {string} haystack to search through
+ * @param search {string} needle to search for
+ * @param [caseSensitive] {boolean} optional boolean to use case-sensitive searching
+ */
+myApp.filter('highlight', function () {
+    return function (text, search, caseSensitive) {
+        if (search || angular.isNumber(search)) {
+            text = text.toString();
+            search = search.toString();
+            if (caseSensitive) {
+                return text.split(search).join('<span class="ui-match">' + search + '</span>');
+            } else {
+                return text.replace(new RegExp(search, 'gi'), '<span class="ui-match">$&</span>');
+            }
+        } else {
+            return text;
+        }
+    };
+});
